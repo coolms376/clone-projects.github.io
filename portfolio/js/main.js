1c86f14a95c8c6d065849a13f6aebe9e2c65b3f2
= <coolms376@gmail.com>
2021-03-19 17:13:05 +0530

    Added logo backgroung & Descrition animation

---
+++ portfolio/js/main.js
@@ -0,0 +1,58 @@
+// var checkbox = document.getElementById("ham");
+
+// checkbox.addEventListener("click",()=>{
+//     if(checkbox.checkbox){
+
+//     }
+
+// })
+
+var
+  words = ['Developer','Programmer','Designer'],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 5,
+  speed = 100;
+
+var wordflick = function(){
+  setInterval(function(){
+      if (forwards) {
+        if(offset >= words[i].length){
+          ++skip_count;
+          if (skip_count == skip_delay) {
+            forwards = false;
+            skip_count = 0;
+          }
+        }
+      }
+      else {
+         if(offset == 0){
+            forwards = true;
+            i++;
+            offset = 0;
+            if(i >= len){
+              i=0;
+            } 
+         }
+      }
+      part = words[i].substr(0, offset);
+      if (skip_count == 0) {
+        if (forwards) {
+          offset++;
+        }
+        else {
+          offset--;
+        }
+      }
+    	$('#steps').text(part);
+  },speed);
+};
+
+$(document).ready(function(){
+  wordflick();
+});
+
